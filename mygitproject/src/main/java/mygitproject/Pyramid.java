77d5f8576736a2584e8e939389907e7bd5a33236
Balaji Venugopal <balaji.2303@gmail.com>
2015-06-10 08:22:36 +0100

    Initial Commit

--- mygitproject/src/main/java/mygitproject/Pyramid.java
+++ mygitproject/src/main/java/mygitproject/Pyramid.java
@@ -9,9 +9,9 @@ package mygitproject;
  */
 public class Pyramid {
 
-	/**
+	/***
 	 * @param args
-	 **/
+	 ***/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 for(int i=1; i<= 5 ;i++){
