083c53659e34109b293af93fd7766034947826c8
Balaji Venugopal <balaji.2303@gmail.com>
2015-06-10 08:05:14 +0100

    Initial Commit

---
+++ mygitproject/src/main/java/mygitproject/Pyramid.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package mygitproject;
+
+/**
+ * @author yt53
+ *
+ */
+public class Pyramid {
+
+	/**
+	 * @param args
+	 **/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1; i<= 5 ;i++){
+            
+            for(int j=0; j < i; j++){
+                    System.out.print("*");
+            }
+           
+            //generate a new line
+            System.out.println("");
+    }
+   
+    //create second half of pyramid
+    for(int i=5; i>0 ;i--){
+           
+            for(int j=0; j < i; j++){
+                    System.out.print("*");
+            }
+           
+            //generate A new line
+            System.out.println("");
+    }
+
+	}
+
+}
